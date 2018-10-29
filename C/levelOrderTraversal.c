ff5fd0cafd3e646b0c207839fe0e3490df159fca
teamkweku <kwekustrings@gmail.com>
2018-10-29 02:51:58 +0000

    Order Traversal

---
+++ C/levelOrderTraversal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    char data;
+    struct node *left, *right;
+};
+
+void levelOrder(struct node* root)
+{
+    if (root == NULL) return;
+
+    //For function to work create a queue of pointer to node
+    
+}
+
+/**********
+int main(void)
+
+*****/
